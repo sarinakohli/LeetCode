c23b22be41e45541864a419b255cf7b1a3217e78
Sarina Kohli <121040421+sarinakohli@users.noreply.github.com>
2024-06-18 14:27:26 +0100

    Add files via upload

---
+++ 9.PalindromeNumbers/Solution.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+class Solution 
+{
+    public static void main(String [] args)
+    {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter a word");
+        int word = s.nextInt();
+        s.close();
+        System.out.println(isPalindrome(word));
+    }
+    public static boolean isPalindrome(int x) 
+        {
+            String xx = Integer.toString(x);
+            for(int i = 0; i <= xx.length()-1; i++)
+            {
+                if(xx.length() == 1)
+                {
+                    return true;
+                }
+                else if(xx.charAt(i) == (xx.charAt(xx.length()-1)))
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+                //xx = xx.substring(1, xx.length()-1);
+          
+            }
+            //System.out.println(xx);
+            return false;
+
+        }
+    
+}
